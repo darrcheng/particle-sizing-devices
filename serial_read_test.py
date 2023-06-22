e7d3168550df8545314aaae5fd84d66dbfbbbb9c
MCC <dhcheng@andrew.cmu.edu>
2023-06-22 13:11:28 -0400

    working serial read test

--- serial_read_test.py
+++ serial_read_test.py
@@ -3,18 +3,23 @@ import csv
 import time
 
 # Set up the serial port
-ser = serial.Serial("COM1", 9600)  # Replace 'COM1' with the name of your serial port
+# ser = serial.Serial("COM5", baudrate = 115200,timeout = 0.05)  # Replace 'COM1' with the name of your serial port
+ser = serial.Serial("COM6",9600,7,"E")
 ser.flushInput()
 
 # Set up the CSV file
 filename = "data.csv"
-header = ["time", "rall", "RSF", "RIF"]  # Customize the header as needed
+#3772
+# header = ["time", "concentration","instrument errors","saturator temp","condensor temp","optics temp", "cabinet temp","ambient pressure","orifice pressure","nozzle pressure","laser current","liquid level","aersol flow", "inlet flow","serial number"]  # Customize the header as needed
+#3025
+header = ['time','concentration','condensor temp','saturator temp','optics temp','flow','ready environment','reference detector voltage','detector voltage','pump control value','1 second counts','liquid level']
 with open(filename, mode="w", newline="") as csv_file:
     writer = csv.writer(csv_file)
     writer.writerow(header)
 
 # Send commands and record responses
-commands = ["rall", "RSF", "RIF"]  # Add more commands as needed
+# commands = ["RALL", "RSF", "RIF","RSN","RCOUNT1","RCOUNT2"]  # Add more commands as needed
+commands = ['RD','R1','R2','R3','R4','R5','R6','R7','RE','RB','R0']
 while True:
     try:
         # Store responses in a list
@@ -22,13 +27,20 @@ while True:
 
         for command in commands:
             # Send command to serial port
-            ser.write((command + "\n").encode())
-
+            # print(1)
+            ser.write((command + "\r").encode())
+            # cmd = "rall\r"
+            # ser.write(cmd.encode())
+            # print(2)
+            # print(ser.readline())
             # Read response from serial port
             response = ser.readline().decode().rstrip()
-
+            response = response.split(",")
+            # print(3)
             # Append response to the list
-            responses.append(response)
+            responses.extend(response)
+
+        print(responses)
 
         # Write responses to CSV file
         with open(filename, mode="a", newline="") as csv_file:
