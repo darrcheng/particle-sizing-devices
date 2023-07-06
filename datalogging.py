fixed bug where inversion used runtime instead of flow, record dndlndp in raw data, fixed a* calculation

datalogging.py
@@ -83,14 +83,14 @@ def dataLogging(stop_threads, b, close_barrier, dma, data_config, voltage_config
                 )
             previous_calc_diameter = calculated_dia
             # Invert data
-            if shared_var.blower_runtime > 0 and shared_var.concentration != -9999:
+            if shared_var.flow_read > 0 and shared_var.concentration != -9999:
                 dndlndp = invert_data(
                     shared_var.concentration,
                     calculated_dia,
                     voltage_config["dma_eff_length"],
                     voltage_config["aerosol_charge"],
                     voltage_config["dma_sample_flow"],
-                    shared_var.blower_runtime * 1000,
+                    shared_var.flow_read * 1000,
                 )
             else:
                 dndlndp = 0
@@ -116,6 +116,7 @@ def dataLogging(stop_threads, b, close_barrier, dma, data_config, voltage_config
                         shared_var.curr_count,
                         shared_var.pulse_width,
                         shared_var.pulse_width_error,
+                        dndlndp,
                         shared_var.blower_runtime,
                         shared_var.voltage_runtime,
                         shared_var.voltage_monitor_runtime,
@@ -275,18 +276,20 @@ def invert_data(N, d_p, l_eff_m, aerosol_charge, q_a_ccm, q_c_ccm):
     q_s = q_a_ccm  # ccm [Aerosol Outlet Flowrate]
     q_c = q_c_ccm  # ccm [Sheath Flowrate]
     q_m = q_c_ccm  # ccm [Excess Flowrate]
-    diameters = np.array([shared_var.low_dia_lim, shared_var.high_dia_lim])
+    dlnDp = shared_var.dlnDp
+    diameters = np.array([np.exp(np.log(d_p) - dlnDp / 2), np.exp(np.log(d_p) - dlnDp / 2)])
     elec_mobility = mobilitycalc.calc_mobility_from_dia(diameters)
     dlnZp = np.log(elec_mobility[1]) - np.log(elec_mobility[0])
-    dlnDp = np.log(diameters[1]) - np.log(diameters[0])
     a_star = -dlnZp / dlnDp
     beta = (q_s + q_a) / (q_m + q_c)
     delta = (q_s - q_a) / (q_s + q_a)
     if d_p < 1.00001:
         d_p = 1.00001
     charge_frac = mobilitycalc.calc_charged_frac(aerosol_charge, d_p)
+    # charge_frac = 1
     cpc_active_eff = 1
     dma_penetration = mobilitycalc.calc_dma_penetration(d_p, l_eff_m, q_a)
+    # dma_penetration = 1
     sample_tube_penetration = 1
     penetrate_eff = dma_penetration * sample_tube_penetration
     dNdlnDp = (N * a_star) / (